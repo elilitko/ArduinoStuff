dad8d6b28d5195a4e124828b57a0587c49a9bc7f
elilitko <elilitko@mail.ru>
2019-07-12 17:59:54 +0300

    Add files via upload

---
+++ VirtualWire2/examples/FrequencyCheck/FrequencyCheck.ino
@@ -0,0 +1,21 @@
+// FrequencyCheck.ino
+//
+// Служебный скетч для проверки частоты таймера и параметров таймера
+//
+// Если в файле VirtualWire2_Config.h назначить пин FREQUENCY_CHECK_PIN
+// (будет сконфигурирован на OUTPUT в vw_setup)
+// То при работе данного скетча на FREQUENCY_CHECK_PIN можно замерять частоту
+// 
+// Если в файле VirtualWire2_Config.h назначить 
+//		#define	REPORT_TIMER_PARAMETERS	Serial
+// То при работе данного скетча в мониторе порта можно будет увидет параметры таймера
+// (делитель частоты и количество тиков)
+// 
+#include <VirtualWire2.h>
+
+void setup(void) {
+	Serial.begin(115200);
+	vw_setup(2000);	// Здесь нужно задать скорость (бит/сек)
+}
+
+void loop(void) {}
