cee3cfd7d824e4a33010995600e932dad6419b93
elilitko <elilitko@mail.ru>
2020-01-17 10:39:21 +0300

    Update Printing.h
    Добавлен макрос printVar

--- Printing/Printing.h
+++ Printing/Printing.h
@@ -1,6 +1,9 @@
 #ifndef	PRINTING_H
 #define	PRINTING_H
 
+// для печати в виде "A=321"
+#define printVar(x) do { Serial.print(#x "=");Serial.println(x); } while (false)
+
 template <typename T> inline Print & operator << (Print &s, T n) { s.print(n); return s; }
 
 #endif	//		PRINTING_H
