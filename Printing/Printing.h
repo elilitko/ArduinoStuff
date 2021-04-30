745cf5aaef9c8fdd5ea4c03249af70db05e91bc4
elilitko <elilitko@mail.ru>
2021-04-30 08:41:10 +0300

    Update Printing.h

--- Printing/Printing.h
+++ Printing/Printing.h
@@ -4,7 +4,7 @@
 // для печати в виде "A=321"
 #define printVar(x) do { Serial << #x "=" << (x) << "\r\n"; } while (false)
 
-template <typename T> inline Print & operator << (Print &s, T n) { s.print(n); return s; }
+template <typename T> inline Print & operator << (Print & s, T & n) { s.print(n); return s; }
 
 #if	((__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 7))) 
 	inline Print & operator << (Print &s, __int24 n) { s.print((int32_t)n); return s; }
